833473d64dc1004a88f6a11a2d940558df93796d
YerayCastro <112658475+YerayCastro@users.noreply.github.com>
2023-11-23 11:40:22 +0100

    Initial Commit

---
+++ Trantor/TrantorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrantorApp.swift
+//  Trantor
+//
+//  Created by Yery Castro on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TrantorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
